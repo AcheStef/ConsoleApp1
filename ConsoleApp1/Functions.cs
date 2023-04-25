99d951b30e5aa27ec17a86c2c8f5240e1096541a
AcheStef <ache.stefanoski@gmail.com>
2023-04-25 20:55:10 +0200

    Voved vo c# functions

---
+++ ConsoleApp1/Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	public class Functions
+	{                                       //"1"
+		public bool Isnumber(string ImeNaVlezenParametarString)
+		{
+			//mesto za logika da proveri dali vlezniot parametar e number ili ne e
+
+
+			return true;
+		}
+	}
+}
